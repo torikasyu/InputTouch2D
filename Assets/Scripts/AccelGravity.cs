e3084339c57b900526d35bc058848f228972578b
torikasyu <tanaka@lyles.jp>
2017-01-16 22:37:33 +0900

    del branch

---
+++ Assets/Scripts/AccelGravity.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelGravity : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private float force = 100.0f; 
+
+	// Use this for initialization
+	void Start () {
+		rb = this.gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// 加速度与える
+		Vector3 dir = Vector3.zero;
+		dir.x = Input.acceleration.x;
+		dir.y = Input.acceleration.y;
+		dir.z = 0;
+
+
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize ();
+
+		dir *= Time.deltaTime;
+		rb.AddForce (dir * force);
+
+
+	}
+
+	void FixedUpdate () {
+	}
+
+}
